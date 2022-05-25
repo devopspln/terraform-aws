2d71cad5b74beef709c0c47d27086d978c560891
Md Maruf Hossain <98268806+devopspln@users.noreply.github.com>
2022-05-25 21:50:20 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,8 @@
+#Variables to be used to create resouces
+variable "product" {}
+variable "environment" {}
+variable "vpc_cidr" {}
+variable "tenancy" {}
+variable "public_subnets" {}
+variable "private_subnets" {}
+variable "availability_zones" {}
